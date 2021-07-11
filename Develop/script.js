2003739675debf474dac23f617017a258f281e10
Kamille <tramia25@gmail.com>
2021-07-11 10:36:11 -0400

    updated function structure

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,6 @@
+   // display current day and time on page
+   $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
 $(document).ready(function () {
     // listen for save button clicks html id 
     $('.saveBtn').on('click', function () {
@@ -7,22 +10,16 @@ $(document).ready(function () {
   
       // save Sets the value of the pair identified by key to value, 
     //   creating a new key/value pair if none existed for key previously.
-// Throws a "QuotaExceededError" DOMException exception if the new value couldn't be set. 
-// (Setting could fail if, e.g., the user has disabled storage for the site, 
-// or if the quota has been exceeded.) 
       localStorage.setItem(time, value);
   
       // Show prompt that item was saved to localStorage by adding class 'show'
       $('.prompt').addClass('show');
   
-      // Timeout to remove 'show' class after 3 seconds
-      setTimeout(function () {
-        $('.prompt').removeClass('show');
-      }, 3000);
+      // // Timeout to remove 'show' class after 3 seconds
+      // setTimeout(function () {
+      //   $('.prompt').removeClass('show');
+      // }, 3000);
     });
-
-     // display current day and time on page
-     $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
   
     function hourUpdater() {
       // get current number of hours
@@ -30,13 +27,16 @@ $(document).ready(function () {
   
       // loop over time field blocks that splits once the string into the string array resulting in 
       $('.time-block').each(function () {
-        var hourBlock = parseInt($(this).attr('id').split('-')[1]);
+        var hourBlock = parseInt($(this).attr('id').split('hours')[1]);
   
         // check if moved pastBlock at this time triggring color update
         if (hourBlock < currentBlock) {
           $(this).addClass('pastBlock');
+          $(this).removeClass("future");
+          $(this).removeClass("present");
         } else if (hourBlock === currentBlock) {
           $(this).removeClass('pastBlock');
+          $(this).removeClass('future');
           $(this).addClass('present');
         } else {
           $(this).removeClass('pastBlock');
@@ -46,10 +46,9 @@ $(document).ready(function () {
       });
     }
   
-    hourUpdater();
   
     // set up interval to check if current time needs to be updated
-    var interval = setInterval(hourUpdater, 15000);
+   setInterval(hourUpdater, 1000);
   
     // load any saved data from localStorage
     $('#hour8 .description').val(localStorage.getItem('hour8'));
@@ -65,6 +64,9 @@ $(document).ready(function () {
     $('#hour18 .description').val(localStorage.getItem('hour18'));
     $('#hour19 .description').val(localStorage.getItem('hour19'));
     $('#hour20 .description').val(localStorage.getItem('hour20'));
+
+
+    hourUpdater()
    
   
   });
